a7b1fa12a26dacb724fd8d4fa747a21da07756a6
Leandro Vandari <bernardokreutz@gmail.com>
2024-08-24 22:10:16 -0300

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub mod vertex;
 
-
 pub fn generate_line_vertices(lines_amount: usize) -> Vec<vertex::Vertex> {
-    let list_length = (lines_amount*4) + 8;
+    let list_length = (lines_amount * 4) + 8;
     let mut list = Vec::with_capacity(list_length);
     for i in 0..lines_amount {
         let x = ((i as f32 + 1.0) / (lines_amount + 1) as f32) * 2.0 - 1.0;
@@ -10,24 +9,22 @@ pub fn generate_line_vertices(lines_amount: usize) -> Vec<vertex::Vertex> {
 
         //vertical
         list.push(vertex::vertex(x, -1.0, 0));
-       list.push(vertex::vertex(x, 1.0, 0));
+        list.push(vertex::vertex(x, 1.0, 0));
 
-       //horizontal
-       list.push(vertex::vertex(-1.0, y, 1));
+        //horizontal
+        list.push(vertex::vertex(-1.0, y, 1));
         list.push(vertex::vertex(1.0, y, 1));
-
-
     }
 
     list.extend([
-            vertex::vertex(-1.0, -1.0, 1),
-            vertex::vertex(1.0, -1.0, 1),
-            vertex::vertex(-1.0, 1.0, 1),
-            vertex::vertex(1.0, 1.0, 1),
-            vertex::vertex(-1.0, 1.0, 0),
-            vertex::vertex(-1.0, -1.0, 0),
-            vertex::vertex(1.0, 1.0, 0),
-            vertex::vertex(1.0, -1.0, 0)
+        vertex::vertex(-1.0, -1.0, 1),
+        vertex::vertex(1.0, -1.0, 1),
+        vertex::vertex(-1.0, 1.0, 1),
+        vertex::vertex(1.0, 1.0, 1),
+        vertex::vertex(-1.0, 1.0, 0),
+        vertex::vertex(-1.0, -1.0, 0),
+        vertex::vertex(1.0, 1.0, 0),
+        vertex::vertex(1.0, -1.0, 0),
     ]);
     list
 }
